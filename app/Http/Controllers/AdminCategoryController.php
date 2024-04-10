acbcecae617a273f5dc4f3a6c6f4f8dc9ac3c1bc
Gung Dika <gungdika88@gmail.com>
2024-04-10 21:26:44 +0800

    adding authorized admin

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -12,7 +12,12 @@ class AdminCategoryController extends Controller
      */
     public function index()
     {
-        //
+        $this->authorize('admin');
+        return view('dashboard.categories.index', [
+            'title' => 'Categories',
+            'active' => 'categories',
+            'categories' => Category::all()
+        ]);
     }
 
     /**
